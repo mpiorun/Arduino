d2229ca8ffc76438fb5cf6d3633507969f11479f
Michał Piorun <mojeczujniki@gmail.com>
2017-12-30 20:46:23 +0100

    Remove pin 9 output

--- BtAtCommands/bt.ino
+++ BtAtCommands/bt.ino
@@ -2,7 +2,6 @@
 SoftwareSerial mySerial(10, 11); // RX, TX
 void setup() {
   Serial.begin(9600);
-  pinMode(9,OUTPUT); digitalWrite(9,HIGH);
   Serial.println("Enter AT commands:");
   mySerial.begin(38400);
 }
